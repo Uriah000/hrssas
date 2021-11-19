a75752a8ff22d9067d8dee6befaec0d0f16041b0
Uriah <tym000212@163.com>
2021-11-19 11:48:51 +0800

    人资项目初始化

---
+++ src/icons/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+requireAll(req)
+// 将所有svg目录下所有的.svg后缀文件全部引入项目中
